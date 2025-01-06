614181059093324a09b661c69f97ac70c2e5e266
djumanov <djumanovdev@gmail.com>
2025-01-06 10:27:20 +0500

    users

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-users = [
-    {
-        "name": "ali",
-        "age": 19
-    },
-    {
-        "name": "vali",
-        "age": 21
-    },
-    {
-        "name": "jon",
-        "age": 20
-    },
-]
+# high-order function
 
-oldest = users[0]
-for user in users[1:]:
-    if oldest['age'] < user['age']:
-        oldest = user
-
-print(oldest)
+filter()
